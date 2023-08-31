175fef189f3833f995afb01e49bdf9381050d0f0
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-08-31 11:50:55 +0300

    commit

---
+++ ElasticSearch.DATA/Entity/Cities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ElasticSearch.DATA.Entity
+{
+    public class Cities
+    {
+        public string Id { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public int Population { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+}
+
